04d8a0e00f5dbb70c6976759ad17abbc535d1ec0
jammychiou1 <jammy.chiou1@gmail.com>
2022-03-06 23:35:31 +0800

    moved QueryClientProvider to index.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import { Users } from './UsersRouting';
 import { NoMatch } from './NoMatch';
-import { QueryClient, QueryClientProvider } from 'react-query';
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      // Failed fetches (e.g. a "Not Found" fetch) are not cached. Therefore, after a Not Found fetch, 
-      // even if stale time is Infinity, react-query will repeatedly call github API if refetch is triggered.
-      // This can cause unwanted API calls. (maybe treat "Not Found" as success in the future?)
-      staleTime: Infinity,
-      retry: false 
-    }
-  }
-});
 
 function Home() {
   return (
@@ -32,13 +20,11 @@ function Home() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="users/*" element={<Users />} />
-        <Route path="*" element={<NoMatch errLocation="App" />} />
-      </Routes>
-    </QueryClientProvider>
+    <Routes>
+      <Route path="" element={<Home />} />
+      <Route path="users/*" element={<Users />} />
+      <Route path="*" element={<NoMatch errLocation="App" />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import './index.css';
 import { App } from './App';
-import reportWebVitals from './reportWebVitals';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // Failed fetches (e.g. a "Not Found" fetch) are not cached. Therefore, after a Not Found fetch, 
+      // even if stale time is Infinity, react-query will repeatedly call github API if refetch is triggered.
+      // This can cause unwanted API calls. (maybe treat "Not Found" as success in the future?)
+      staleTime: Infinity,
+      retry: false 
+    }
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
